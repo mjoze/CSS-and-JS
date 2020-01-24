8409a0f9617e4e81f53c81cc90a194f0433a0ce4
mjoze <mirek.jozefiak@gmail.com>
2020-01-24 22:02:10 +0100

    style_css_added

--- project_fetch/main.js
+++ project_fetch/main.js
@@ -14,8 +14,22 @@ const getUsers = (e) => {
                 return response.json()
             }
         })
-        .then(json => console.log(json))
+        .then(json => showUsers(json.results))
         .catch(err => console.log(err))
+}
+
+const showUsers = (users) => {
+    const resultArea = document.querySelector('.user-list');
+    resultArea.textContent = "";
+    users.forEach(user => {
+        const item = document.createElement('div');
+        item.className = 'user';
+        item.innerHTML = `
+        <div class="user__name">${user.name.title.toUpperCase()} ${user.name.last.toUpperCase()}</div>
+        <img class="user__image" src=${user.picture.medium}>
+        `
+        resultArea.appendChild(item)
+    });
 }
 
 
