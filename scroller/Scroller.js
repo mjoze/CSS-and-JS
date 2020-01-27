60f35c6784bb2abba15591e5cebb67a7730a3f70
mjoze <mirek.jozefiak@gmail.com>
2020-01-27 18:41:15 +0100

    add browser compatibilty

--- scroller/Scroller.js
+++ scroller/Scroller.js
@@ -33,12 +33,12 @@ class Scroller {
             this.isThrottled = false;
         }, 1000)
 
-        const direction = event.wheelDelta < 0 ? 1 : -1;
+        const direction = event.deltaY > 0 ? 1 : -1;
 
         this.scroll(direction);
     }
 
-    scroll = (direction) => {
+    scroll(direction) {
 
         if (direction === 1) {
             const isLastSection = this.currentSectionIndex === this.sections.length - 1;
@@ -52,14 +52,14 @@ class Scroller {
         this.scrollToCurrentSection();
     }
 
-    scrollToCurrentSection = () => {
+    scrollToCurrentSection() {
         this.selectActiveNavItem();
         this.sections[this.currentSectionIndex].scrollIntoView({
             behavior: 'smooth',
             block: "start",
         })
     }
-    drawNavigation = () => {
+    drawNavigation() {
         this.navigationContainer = document.createElement('aside');
         this.navigationContainer.setAttribute('class', 'scroller__navigation');
         const list = document.createElement('ul');
@@ -81,7 +81,7 @@ class Scroller {
 
     }
 
-    selectActiveNavItem = () => {
+    selectActiveNavItem() {
         if (this.navigationContainer) {
             const navigationItems = this.navigationContainer.querySelectorAll('li');
             navigationItems.forEach((item, index) => {
--- scroller/index.js
+++ scroller/index.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
     const scroller = new Scroller('.root')
-    document.addEventListener('mousewheel', scroller.listenScroll)
+    document.addEventListener('wheel', (event) => scroller.listenScroll(event))
 })
