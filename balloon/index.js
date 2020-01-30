01014e29dacd064948575346067b280f88d471e4
mjoze <mirek.jozefiak@gmail.com>
2020-01-30 15:15:28 +0100

    create class and use

--- balloon/index.js
+++ balloon/index.js
@@ -1,25 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const elements = document.querySelectorAll('.anim-bg');
-
-    document.addEventListener('mousemove', function (event) {
-        const {
-            clientX,
-            clientY,
-        } = event;
-
-        const centerX = window.innerWidth / 2;
-        const centerY = window.innerHeight / 2;
+    const animBg = new Animbg('.anim-bg')
 
 
-        elements.forEach(element => {
-            const ratioX = -element.getAttribute('ratioX');
-            const ratioY = -element.getAttribute('ratioY');
+    // const elements = document.querySelectorAll('.anim-bg');
 
-            const moveX = clientX - centerX;
-            const moveY = clientY - centerY;
+    document.addEventListener('mousemove', (event) => animBg.listenCursorMove(event))
 
 
-            element.style.transform = `translate(${moveX * ratioX}px, ${moveY * ratioY}px)`
-        })
-    })
+    // document.addEventListener('mousemove', function (event) {
+    //     const {
+    //         clientX,
+    //         clientY,
+    //     } = event;
+
+    //     const centerX = window.innerWidth / 2;
+    //     const centerY = window.innerHeight / 2;
+
+
+    //     elements.forEach(element => {
+    //         const ratioX = -element.getAttribute('ratioX');
+    //         const ratioY = -element.getAttribute('ratioY');
+
+    //         const moveX = clientX - centerX;
+    //         const moveY = clientY - centerY;
+
+
+    //         element.style.transform = `translate(${moveX * ratioX}px, ${moveY * ratioY}px)`
+    //     })
+    // })
 })
