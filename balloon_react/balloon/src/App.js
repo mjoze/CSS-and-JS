96c78e6f018d3280fc1b78573538d9aca74b283b
mjoze <mirek.jozefiak@gmail.com>
2020-01-30 17:04:30 +0100

    react app

--- balloon_react/balloon/src/App.js
+++ balloon_react/balloon/src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Animated from './Animated'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="balloon-animation">
+        <Animated
+          className="flying-cloud"
+          src='/cloud1.png'
+          ratioX="0.36"
+          ratioY="0.34"
+        />
+        <Animated
+          className="flying-cloud"
+          src='/cloud2.png'
+          ratioX="0.2"
+          ratioY="0.23"
+        />
+        <Animated
+          className="flying-cloud"
+          src='/cloud1.png'
+          ratioX="0.26"
+          ratioY="0.34"
+        />
+        <Animated
+          className="flying-cloud"
+          src='/cloud2.png'
+          ratioX="0.1"
+          ratioY="0.11"
+        />
+        <Animated
+          className="flying-cloud"
+          src='/cloud1.png'
+          ratioX="0.13"
+          ratioY="0.53"
+        />
+        <Animated
+          className="flying-cloud"
+          src='/cloud2.png'
+          ratioX="0.23"
+          ratioY="0.41"
+        />
+
+        <Animated
+          className="flying-balloon"
+          src='/balloon.png'
+          ratioX="0.07"
+          ratioY="0.3"
+        />
+      </div>
     </div>
   );
 }
