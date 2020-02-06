244ef6053b17ed4f26c83256b73ebaba9364be89
mjoze <mirek.jozefiak@gmail.com>
2020-02-06 20:05:19 +0100

    shortCode

---
+++ node.js/first-code.js
@@ -0,0 +1,16 @@
+const a = () => {
+    console.log('function a');
+};
+
+console.log("hello");
+
+
+setTimeout(
+    () => {
+        const text = 'done'
+        console.log(text);
+    }, 1000
+)
+
+console.log('end');
+a();
