259c17a9b6d96c375386df92a41eaa739ecd6fb1
mjoze <mirek.jozefiak@gmail.com>
2020-03-07 20:37:50 +0100

    add buttons

--- gallery/gallery/js/app.js
+++ gallery/gallery/js/app.js
@@ -4,7 +4,8 @@ class Doggo {
     constructor() {
         this.apiUrl = 'https://dog.ceo/api';
         this.imgEl = document.querySelector('.featured-dog img');
-        this.backgroundEl = document.querySelector('.featured-dog__background')
+        this.backgroundEl = document.querySelector('.featured-dog__background');
+        this.tilesEl = document.querySelector('.tiles')
         this.init();
     }
 
@@ -32,8 +33,50 @@ class Doggo {
                 this.backgroundEl.style.background = `url("${src}")`
             });
 
+        this.showAllBreeds();
+    }
+
+    addBreed(breed, subBreed) {
+        let name;
+        let type;
+
+        if (typeof subBreed === 'undefined') {
+            name = breed;
+            type = breed;
+        } else {
+            name = `${breed} ${subBreed}`;
+            type = `${breed}/${subBreed}`;
+        }
+        const tile = document.createElement('div');
+        tile.classList.add('tiles__tile');
+
+        const tileContent = document.createElement('div');
+        tileContent.classList.add('tiles__tile-content');
+        tileContent.innerText = name;
+        tileContent.addEventListener('click', () => {
+            this.getRandomImageByBreed(type)
+                .then(src => {
+                    this.imgEl.setAttribute('src', src)
+                    this.backgroundEl.style.background = `url("${src}")`
+                });
+        });
+        tile.appendChild(tileContent);
+        this.tilesEl.appendChild(tile)
+    }
+
+    showAllBreeds() {
         this.listBreeds()
-            .then(breeds => console.log(breeds));
+            .then(breeds => {
+                for (const breed in breeds) {
+                    if (breeds[breed].length === 0) {
+                        this.addBreed(breed);
+                    } else {
+                        for (const subBreed of breeds[breed]) {
+                            this.addBreed(breed, subBreed);
+                        }
+                    }
+                }
+            })
     }
 };
 
