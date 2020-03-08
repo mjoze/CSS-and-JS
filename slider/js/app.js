583f8448984a66f8f8f88c7be111ea74e6627cc6
mjoze <mirek.jozefiak@gmail.com>
2020-03-08 13:45:46 +0100

    add responsivenss

--- slider/js/app.js
+++ slider/js/app.js
@@ -1,1 +1,19 @@
-import '../sass/style.scss'
+import '../sass/style.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const imagesContainerEl = document.querySelector('.slider__images-container');
+    const imag1El = document.querySelector('.slider__image-container--first img');
+    const imag2El = document.querySelector('.slider__image-container--second');
+
+    function adjustImagesSize() {
+        const imagesCOntainerWidth = imagesContainerEl.offsetWidth;
+        imag1El.style.width = imagesCOntainerWidth + 'px';
+        imag2El.style.width = imagesCOntainerWidth + 'px';
+    }
+
+
+    window.addEventListener('resize',
+        adjustImagesSize);
+    adjustImagesSize();
+
+})
