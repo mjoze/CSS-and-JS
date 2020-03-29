13546abe042286d1ea2027a4d75112bfc3d11481
mjoze <mirek.jozefiak@gmail.com>
2020-03-29 19:28:23 +0100

    change if in find method

--- JS/js30/7/index.js
+++ JS/js30/7/index.js
@@ -44,17 +44,18 @@
   // Array.prototype.some() // is at least one person 19 or older?
   const date = new Date();
   const year = date.getFullYear();
+  const currentYear = (new Date()).getFullYear();
+
   const olderThan19 = people.some(person =>
-      year - person.year > 19)
+      year - person.year >= 19)
   //   console.log(olderThan19);
   // Array.prototype.every() // is everyone 19 or older?
-  const olderThan19orEqual = people.every(person => year - person.year >= 19);
+  const olderAllThan19 = people.every(person => year - person.year >= 19);
 
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
-  const commentID823423 = comments.find(comment => comment.id === 823423 ? comment.text : '');
-
+  const commentID823423 = comments.find(comment => comment.id === 823423);
   // Array.prototype.findIndex()
   // Find the comment with this ID
   const commentIndex = comments.findIndex(comment => comment.id === 823423);
