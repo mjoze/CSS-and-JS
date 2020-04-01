9530d06b9e440fb7623d5de4ded3d5fad052512d
mjoze <mirek.jozefiak.gmail.com>
2020-04-01 12:50:03 +0200

    renamed data

---
+++ webpack_training/example3/config/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+// import path from 'path';
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, '../', 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: "raw-loader",
+      }
+    ]
+  }
+}
---
+++ webpack_training/example3/src/index.js
@@ -0,0 +1,6 @@
+import { message, messageDOM } from './message';
+import info from './title.txt';
+
+
+message(info);
+messageDOM(info);
